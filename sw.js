cadac962da7fc17245728e170ed1fdbf4ec34498
christmasgo7862-glitch <christmasgo7862@gmail.com>
2026-09-11 14:32:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-const CACHE='gongmu-insight-pro-v3-1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>{e.waitUntil((async()=>{try{
+const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));
+await self.registration.unregister();
+const cs=await self.clients.matchAll({type:'window',includeUncontrolled:true});
+for(const c of cs){try{c.navigate(c.url)}catch(_){}}}catch(_){}})())});
+self.addEventListener('fetch',()=>{});
